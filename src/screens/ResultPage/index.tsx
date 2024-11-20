395cc3ab445b55e0a010b57d862a554f30b67c60
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-11-20 16:29:54 +0530

    result ui

--- src/screens/ResultPage/index.tsx
+++ src/screens/ResultPage/index.tsx
@@ -16,7 +16,7 @@ const ResultPage = () => {
       <div className="max-w-4xl mx-auto p-4 mt-36">
         <Card className="w-full">
           <CardHeader>
-            <CardTitle className="text-xl md:text-2xl flex items-center justify-between">
+            <CardTitle className="text-xl md:text-2xl flex flex-wrap items-center justify-between">
               Result : Math Quiz App 🎉
               <Button>
                 Leaderboard <Rss />
@@ -25,11 +25,30 @@ const ResultPage = () => {
           </CardHeader>
           <CardContent>
             {resultData[0]?.points ? (
-              <>
+              <div>
                 <h2 className="text-2xl mb-2">
                   Points : {resultData[0]?.points}
                 </h2>
-              </>
+                <div className="flex justify-between items-center border-b py-2 bg-green-900 p-2">
+                  <p>Question</p>
+                  <p>Correct Answer</p>
+                  <p>User Answer</p>
+                  <p>Result</p>
+                </div>
+                {resultData[0]?.updatedQuizOutput.map((rs, idx) => (
+                  <div
+                    key={idx}
+                    className="flex justify-between items-center border-b py-2 p-2"
+                  >
+                    <p>{rs.question}</p>
+                    <p>{rs.correctAnswer}</p>
+                    <p>{rs.userAnswer}</p>
+                    <p>
+                      {rs.correctAnswer === rs.userAnswer ? "Correct" : "Wrong"}
+                    </p>
+                  </div>
+                ))}
+              </div>
             ) : (
               <>
                 <p>Play Again to get Results</p>
