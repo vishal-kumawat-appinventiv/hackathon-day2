8a6a261d3d26fca2cd4682fc45b74129cf80df2d
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-11-20 11:45:06 +0530

    onboarding ui

--- src/screens/OnboardingPage/index.tsx
+++ src/screens/OnboardingPage/index.tsx
@@ -1,8 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ArrowRight } from "lucide-react";
+import { useState } from "react";
+import { Label } from "@/components/ui/label";
 
 const OnboardingPage = () => {
-  return (
-    <div>OnboardingPage</div>
-  )
-}
+  const [username, setUsername] = useState("");
 
-export default OnboardingPage
+  const handleOnboarding = () => {
+    console.log("username", username);
+  };
+
+  return (
+    <div className="p-4 max-w-xl mx-auto mt-10 md:mt-48">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="text-xl md:text-2xl">
+            Onbording : Math Quiz App
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Label htmlFor="username">Enter your username</Label>
+          <Input
+            placeholder="Enter here.."
+            className="mt-2"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handleOnboarding}>
+            Proceed <ArrowRight />
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default OnboardingPage;
