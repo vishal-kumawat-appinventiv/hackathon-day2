4aacf0e339973b34c08719e96a660c43cf144a81
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-11-20 13:55:53 +0530

    questions to asked

--- src/redux/slices/questionSlice.ts
+++ src/redux/slices/questionSlice.ts
@@ -1,6 +1,7 @@
 import { MOCK_QUESTION } from "@/lib/mock";
 import { QuestionType } from "@/lib/types";
 import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
 interface questionState {
   data: QuestionType[];
@@ -30,17 +31,17 @@ export default questionSlice.reducer;
 export const {} = questionSlice.actions;
 
 export const getHardQuestions = createSelector(
-  (state: { question: questionState }) => state.question.data,
+  (state: RootState) => state.questions.data,
   (questions) => questions.filter((question) => question.difficulty === "Hard")
 );
 
 export const getMediumQuestions = createSelector(
-  (state: { question: questionState }) => state.question.data,
+  (state: RootState) => state.questions.data,
   (questions) =>
     questions.filter((question) => question.difficulty === "Medium")
 );
 
 export const getEasyQuestions = createSelector(
-  (state: { question: questionState }) => state.question.data,
+  (state: RootState) => state.questions.data,
   (questions) => questions.filter((question) => question.difficulty === "Easy")
 );
--- src/screens/QuizPage/index.tsx
+++ src/screens/QuizPage/index.tsx
@@ -1,4 +1,22 @@
+import { QuestionType } from "@/lib/types";
+import {
+  getEasyQuestions,
+  getHardQuestions,
+  getMediumQuestions,
+} from "@/redux/slices/questionSlice";
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+
 const QuizPage = () => {
+  const { difficulty } = useSelector((state: RootState) => state.user.data);
+
+  let questionsToBeAsked: QuestionType[] = [];
+
+  if (difficulty === "Easy") questionsToBeAsked = useSelector(getHardQuestions);
+  if (difficulty === "Medium")
+    questionsToBeAsked = useSelector(getMediumQuestions);
+  if (difficulty === "Hard") questionsToBeAsked = useSelector(getEasyQuestions);
+
   return <div>QuizPage</div>;
 };
 
